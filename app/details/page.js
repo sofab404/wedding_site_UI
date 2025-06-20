7a87b55212cb08d8fb0b70d428cb724dc273958e
Fablihahossain <fablihahossain27@gmail.com>
2025-06-20 17:54:25 -0400

    initial pages for routing through navigation

---
+++ app/details/page.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+export default function Details() {
+    return (
+        <>
+        <h1>Our Receptin Details</h1>
+        <br></br>
+        <div>
+            <p>Terrace on the Park</p>
+            <p>5211 111th Street Flushing Meadows Park</p>
+            <p>Queens, NY 11368</p>
+        </div>
+
+        <br></br>
+        <div>
+            Check back again for event schedule!
+        </div>
+        <br></br>
+        <div>
+            <p>Parking located at Venue</p>
+            <p>For Directions to venue, text “TOP” to 208-216-1688</p>
+        </div>
+        </>
+    )
+}
--- app/page.js
+++ app/page.js
@@ -1,8 +1,10 @@
-import TestComponent from "@/testComponent";
+import TestComponent from "@/components/testComponent";
+import Navbar from "@/components/navbar";
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <Navbar></Navbar>
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <TestComponent />
 
@@ -10,9 +12,9 @@ export default function Home() {
 
         </div>
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
+      {/* <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
         <p>Test Footer</p>
-      </footer>
+      </footer> */}
     </div>
   );
 }
