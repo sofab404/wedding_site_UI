27626ae4a6186befb2453c08c827576346aa4990
Fablihahossain <fablihahossain27@gmail.com>
2025-06-27 17:06:50 -0400

    modifications to home screen

--- app/page.js
+++ app/page.js
@@ -1,12 +1,12 @@
-import TestComponent from "@/components/testComponent";
+import HomeComponent from "@/components/homeComponent";
 import Navbar from "@/components/navbar";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-15">
       <Navbar></Navbar>
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <TestComponent />
+        <HomeComponent />
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
 
