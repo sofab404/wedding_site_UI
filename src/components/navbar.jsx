f2c550b63c58709a77d36075d3c394b98f89d804
Fablihahossain <fablihahossain27@gmail.com>
2025-06-27 17:07:38 -0400

    adding navbar to pages and adding new home nav button

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -4,19 +4,22 @@ const Navbar = () => {
   return (
     <>
       <div className="font-[March_Rough] text-[22px] font-bold flex gap-16">
-        <div className="flex-none">
+        <div className="flex-auto cursor-pointer">
+          <a href="/">Home</a>
+        </div>
+        <div className="flex-none cursor-pointer">
           <a href="/story">Our Story</a>
         </div>
-        <div className="flex-auto">
+        <div className="flex-auto cursor-pointer">
           <a href="/rsvp">RSVP</a>
         </div>
-        <div className="flex-auto">
+        <div className="flex-auto cursor-pointer">
           <a href="/details">Details</a>
         </div>
-        <div className="flex-auto">
+        <div className="flex-auto cursor-pointer">
           <a href="/registry">Registry</a>
         </div>
-        <div className="flex-auto">
+        <div className="flex-auto cursor-pointer">
           <a href="/faqs">FAQs</a>
         </div>
       </div>
