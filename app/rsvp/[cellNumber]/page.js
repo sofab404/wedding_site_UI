7593c3283d1a8ba905e7a6c4b54fdb2b2803ea71
Fablihahossain <fablihahossain27@gmail.com>
2025-07-01 23:33:30 -0400

    rsvp dynamic routing

---
+++ app/rsvp/[cellNumber]/page.js
@@ -0,0 +1,14 @@
+'use client'
+import React from "react";
+import Navbar from "@/components/navbar";
+
+export default function RSVP({params}) {
+    const { cellNumber } = React.use(params)
+    return (
+        <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-15">
+        <Navbar />
+        <p> this will be our rsvp page</p>
+        <p>Cell: {cellNumber}</p>
+    </div>
+    )
+}
--- app/rsvp/page.js
+++ app/rsvp/page.js
@@ -1,11 +1,13 @@
-import React from "react";
-import Navbar from "@/components/navbar";
+import React from 'react';
+import Navbar from '@/components/navbar';
 
-export default function RSVP() {
+export default function MainPage() {
     return (
+
         <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-15">
         <Navbar />
-        <p> this will be our rsvp page</p>
+        <p>please enter your phone number here </p>
+
     </div>
     )
 }
