3f29a5092d8ae5799a1e34a029cec4efc3390b51
Fablihahossain <fablihahossain27@gmail.com>
2025-07-08 15:53:09 -0400

    RSVP form logic and functionality

--- app/rsvp/page.js
+++ app/rsvp/page.js
@@ -1,12 +1,61 @@
-import React from 'react';
+'use client'
+import React, {useEffect, useState} from 'react';
 import Navbar from '@/components/navbar';
+import { useRouter } from 'next/navigation';
 
 export default function MainPage() {
+    const router = useRouter();
+    const [cellNumber, setCellNumber] = useState("");
+    const [routeToPage, setRouteToPage] = useState(false);
+    const [loadingMessage, setLoadingMessage] = useState("") 
+    const [invalidResponse, setInvalidResponse] = useState(false);
+
+    const toggleRoute = () => {
+        if(!(cellNumber.length === 0)) {
+            setRouteToPage(true)
+        }
+
+        const regex = /^\d+$/;
+        if(!regex.test(cellNumber)) {
+            setInvalidResponse(true)
+            setRouteToPage(false)
+        }
+    }
+
+    const updateCellNumber = (e) => {
+        setCellNumber(e.target.value)
+    }
+
+    useEffect(() => {
+        if(!routeToPage) {
+            return
+        }
+        else {
+            if(!(cellNumber.length === 0)) {
+                setLoadingMessage("Loading your details...")
+                router.replace(`/rsvp/${cellNumber}`)
+            }
+        }
+    }, [cellNumber, router, routeToPage])
     return (
 
         <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-15">
         <Navbar />
-        <p>please enter your phone number here </p>
+
+        <div className='grid'>
+        <label className='text-[20px] text-center'>Please Enter Your Phone Number for RSVP!</label> <br></br>
+        <input className="rounded-lg border border-gray-300 bg-gray-50 px-4 py-3 text-gray-900 text-base placeholder-gray-500 placeholder-italic shadow-sm focus:border-indigo-400 focus:ring-2 focus:ring-indigo-200 focus:outline-none text-center" 
+            type="tel" 
+            required
+            onChange={(e) => updateCellNumber(e)}>
+        </input>
+        {invalidResponse ? <p className='text-red-600 text-center'>Please enter phone number with only digits and no dashes!</p>: <></>}
+        
+            
+        <br></br>
+        <button className="text-[20px] cursor-pointer border border-black-300 bg-white hover:bg-gray-200" onClick={toggleRoute}>Enter</button>
+        <p className='text-center'>{loadingMessage}</p>
+        </div>
 
     </div>
     )
