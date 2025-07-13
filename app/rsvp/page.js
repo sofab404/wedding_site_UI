2d99e35ae2cadf7a1956ca6db440ad1a1ee72bb6
Fablihahossain <fablihahossain27@gmail.com>
2025-07-13 16:56:15 -0400

    adding country code to rsvp initial form

--- app/rsvp/page.js
+++ app/rsvp/page.js
@@ -5,11 +5,17 @@ import CryptoJS from "crypto-js";
 
 export default function MainPage() {
     const router = useRouter();
+    const countries = [
+       { code: "US", dial: "+1"},
+       { code: "BD", dial: "+880"}
+    ]
+
     const [cellNumber, setCellNumber] = useState("");
     const [routeToPage, setRouteToPage] = useState(false);
     const [loadingMessage, setLoadingMessage] = useState("") 
     const [invalidResponse, setInvalidResponse] = useState(false);
     const [invalidCell, setInvalidCell] = useState(false);
+    const [dialCode, setDialCode] = useState(countries[0].dial)
 
     const toggleRoute = async() => {
         const regex = /^\d+$/;
@@ -67,13 +73,30 @@ export default function MainPage() {
 
         <div className='grid'>
         <label className='text-[20px] text-center'>Please Enter Your Phone Number for RSVP!</label> <br></br>
-        <input className="rounded-lg border border-gray-300 bg-gray-50 px-4 py-3 text-gray-900 text-base placeholder-gray-500 placeholder-italic shadow-sm focus:border-indigo-400 focus:ring-2 focus:ring-indigo-200 focus:outline-none text-center" 
+
+        <div className="flex relative">
+          <select className="absolute border-gray-600 bg-white border border-gray-200 shadow rounded-md max-h-40 overflow-auto h-full">
+            {countries.map((c) => (
+              <option
+                key={c.dial}
+                onClick={() => { setDialCode(c.dial) }}
+                className="px-4 py-2 hover:bg-gray-100 cursor-pointer"
+              >
+                {c.code} {c.dial}
+              </option>
+            ))}
+          </select>
+
+
+        <input className="rounded-lg border border-gray-300 bg-gray-50 px-4 py-3 text-gray-900 text-base placeholder-gray-500 placeholder-italic shadow-sm focus:border-indigo-400 focus:ring-2 focus:ring-indigo-200 focus:outline-none text-center w-full" 
             type="tel" 
             required
             onChange={(e) => updateCellNumber(e)}>
         </input>
+
+        </div>
         {invalidResponse ? <p className='text-red-600 text-center'>Please enter phone number with only digits and no dashes!</p>: <></>}
-        {invalidCell ? <p className='text-red-600 text-center'>Invalid Entry! Number does not exist in our database. <br></br>Please try another number</p>: <></>}
+        {invalidCell ? <p className='text-red-600 text-center'>Invalid Entry! Number does not exist in our database. <br></br>Please try again or try another number</p>: <></>}
         
             
         <br></br>
